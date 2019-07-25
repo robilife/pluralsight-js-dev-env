c3a5c4586ed3902f3fa5e7e465dff1b9e39e7dd8
khk <khk@optesis.com>
2019-07-25 14:27:46 +0100

    [UPDATE] implementing ESLINT

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import numeral from "numeral";
 
 const courseValue = numeral(100).format('$0,0.00');
 //debugger;
-console.log(`I would pay ${courseValue} for this awsome course!`);
+console.log(`I would pay ${courseValue} for this awsome course!`); // eslint-disable-line no-console
